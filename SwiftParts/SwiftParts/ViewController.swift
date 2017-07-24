7f93c40f36efbbff8904738b70f7ba6956027264
motaHack <firstacom_y9emo@icloud.com>
2017-07-24 23:37:30 +0900

    first commit

---
+++ SwiftParts/SwiftParts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftParts
+//
+//  Created by YS on 2017/07/24.
+//  Copyright © 2017年 YS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
